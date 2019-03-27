9c189a2de017948545ddab9efe1ea5bdfc8724ef
dyyyl <dmbromell@gmail.com>
2019-03-27 19:12:33 +0100

    styles 💅🏽

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { Auth0Lock } from 'auth0-lock';
 import React, { Fragment, useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 import GlobalStyle from './styles/GlobalStyle';
 
@@ -50,25 +51,58 @@ const App = () => {
     }
   }, []);
 
+  const Main = styled.main`
+    height: 100vh;
+    background-image: linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `;
+
+  const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+  `;
+
+  const Text = styled.p`
+    font-size: 5rem;
+    color: snow;
+  `;
+
+  const Button = styled.button`
+    height: 3.5vmax;
+    border: 2px solid #0099cc;
+    border-radius: 15px;
+    width: 50%;
+    font-size: 2rem;
+
+    &:hover {
+      transition: all 0.05s ease-in;
+      background: #0099cc;
+      color: snow;
+      text-decoration: none;
+    }
+  `;
+
   return (
     <Fragment>
-      <header>
+      <Main>
         {accessToken ? (
-          <div>
-            <p>{profile.name} is logged in</p>
-            <button type="button" onClick={logout}>
+          <Container>
+            <Text>{profile.name} is logged in</Text>
+            <Button type="button" onClick={logout}>
               Logout
-            </button>
-          </div>
+            </Button>
+          </Container>
         ) : (
-          <div>
-            <p>User is not logged in</p>
-            <button type="button" onClick={showLogin}>
+          <Container>
+            <Text>User is not logged in</Text>
+            <Button type="button" onClick={showLogin}>
               Login
-            </button>
-          </div>
+            </Button>
+          </Container>
         )}
-      </header>
+      </Main>
       <GlobalStyle />
     </Fragment>
   );
