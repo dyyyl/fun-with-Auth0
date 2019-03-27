4fd327482f526524b38868e66052939011c4596c
dyyyl <dmbromell@gmail.com>
2019-03-27 20:55:12 +0100

    hello, world!

--- server/src/hello_world/handler.js
+++ server/src/hello_world/handler.js
@@ -1,9 +1,12 @@
-export const hello = async (event) => {
+export const hello = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
     }),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
   };
 };
